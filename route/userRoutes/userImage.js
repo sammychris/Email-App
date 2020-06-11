bf6d34cb6cb8d1d90565418aebecdf9b859e78df
sammychris <ebusameric@gmail.com>
2020-06-11 18:17:42 +0100

    user to upload image

---
+++ route/userRoutes/userImage.js
@@ -0,0 +1,9 @@
+const db = require('../../models');
+
+function upload(req, res) {
+    const { file } = req;
+    console.log(file);
+    res.json({ link: '/uploads/'+ file.filename });
+}
+
+module.exports = { upload };
