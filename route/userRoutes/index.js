9b352e88aad70ec623b0e1ee238fe0f12d1caf80
sammychris <ebusameric@gmail.com>
2020-06-13 17:24:01 +0100

    import user modules

---
+++ route/userRoutes/index.js
@@ -0,0 +1,16 @@
+const userCampaign 		= require('./userCampaign');
+const userList			= require('./userList');
+const userImage     	= require('./userImage');
+const userMessage   	= require('./userMessage');
+const userSendCampaign	= require('./userSendCampaign');
+const user 				= require('./user');
+
+
+module.exports = {
+	userCampaign,
+	userList,
+	userImage,
+	userMessage,
+	userSendCampaign,
+	user,
+};
