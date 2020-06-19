4d9a56cc2272eb2ae5cb3c270b61d5dbef55426b
sammychris <ebusameric@gmail.com>
2020-06-19 13:36:43 +0100

    features: export modules

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const Campaign    = require('./Campaign');
+const EmailList   = require('./EmailList');
+const Message     = require('./Message');
+const CreateMail  = require('./CreateMail');
+const CheckMail   = require('./CheckMail');
+const User   	  = require('./User');
+
+
+module.exports = { Campaign, EmailList, Message, CreateMail, CheckMail, User };
