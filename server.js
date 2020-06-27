f08e17b1bc5841d7b13ceb462bf8aa5c66380e19
sammychris <ebusameric@gmail.com>
2020-06-27 18:30:48 +0100

    features: add more features

--- server.js
+++ server.js
@@ -2,12 +2,35 @@ require('dotenv').config();
 const express = require('express');
 const mysql = require('mysql2');
 const path = require('path');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+
+
+const { DB_URL, PORT, NODE_ENV } = process.env;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(DB_URL, {useNewUrlParser: true});
+
+const db = mongoose.connection; // Connecting to DATABASE...
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('db connected');
+});
 
 const app = express();
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = NODE_ENV === 'production';
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(cors()) // Use this after the variable declaration
+
+app.use(cookieParser()) // cookie parser..
+
+// Initiallize Passport...
+app.use(passport.initialize());
 
 
 // serving static files...
@@ -31,6 +54,6 @@ if (isProduction) {
 }
 
 
-const listener = app.listen(process.env.PORT, function() {
+const listener = app.listen(PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
