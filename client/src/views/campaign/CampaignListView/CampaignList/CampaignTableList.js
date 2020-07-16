94acb2d04befa47d90b41807bab782352ba4cd84
sammychris <ebusameric@gmail.com>
2020-07-16 20:08:03 +0100

    features: add campaign table list

---
+++ client/src/views/campaign/CampaignListView/CampaignList/CampaignTableList.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+import {
+  Box,
+  makeStyles,
+  Tooltip
+} from '@material-ui/core';
+
+import { X as CancelIcon} from 'react-feather';
+
+import TableList  from './TableList';
+import ListStatus from './ListStatus';
+
+const useStyles = makeStyles((theme) => ({
+  root: {}
+}));
+
+const CampaignTableList = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Box mb={5}>
+      <ListStatus openAndClose={props.openAndClose} />
+      <TableList />
+    </Box>
+  );
+};
+
+export default CampaignTableList;
