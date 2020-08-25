a7ffaa63a44e51e85967dcd0801997af87f4ae5c
sammychris <ebusameric@gmail.com>
2020-08-25 18:35:18 +0100

    features: add a login

---
+++ client/src/views/auth/LoginView.js
@@ -0,0 +1,224 @@
+import React from 'react';
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+  makeStyles
+} from '@material-ui/core';
+import FacebookIcon from 'src/icons/Facebook';
+import GoogleIcon from 'src/icons/Google';
+import Page from 'src/components/Page';
+import AlertMessage from './AlertMessage';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.background.dark,
+    height: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
+}));
+
+const LoginView = () => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const [response, setResponse] = React.useState(false);
+  let   [clickButton, setClick] = React.useState(0);
+
+
+  const handleAlert = () => {
+    setResponse(true);
+    setClick(clickButton + 1);
+  }
+
+  return (
+    <Page
+      className={classes.root}
+      title="Login"
+    >
+      <Box
+        display="flex"
+        flexDirection="column"
+        height="100%"
+        justifyContent="center"
+      >
+        <Container maxWidth="sm">
+          <Formik
+            initialValues={{
+              email: 'demo@devias.io',
+              password: 'Password123'
+            }}
+            validationSchema={Yup.object().shape({
+              email: Yup.string().email('Must be a valid email').max(255, 'Email Too Long!').required('Email is required'),
+              password: Yup.string().min(2, 'Password Too Short!').max(255, 'Password Too Long!').required('Password is required')
+            })}
+            onSubmit={(values, { setSubmitting, setStatus } ) => {
+
+              const { email, password } = values;
+              fetch('/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password })
+              })
+              .then(res => res.json())
+              .then(res => {
+                const { user } = res;
+                if(user){
+                  localStorage.setItem('user', JSON.stringify(res.user || false));
+                  localStorage.setItem('url', res.url || false);
+                  localStorage.setItem('isGoogleVerified', res.isGoogleVerified || false);
+                  setStatus('You have successfully logged in!');
+                  return navigate('/app/dashboard', { replace: true });
+                }
+                setStatus('The lord is my shield!');
+                handleAlert();
+                setSubmitting(false);
+              })
+              .catch(err => console.log(err));
+
+            }}
+          >
+            {({
+              errors,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+              isSubmitting,
+              touched,
+              values,
+              status
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Box mb={3}>
+                  <Typography
+                    color="textPrimary"
+                    variant="h2"
+                  >
+                    Sign in
+                  </Typography>
+                  <Typography
+                    color="textSecondary"
+                    gutterBottom
+                    variant="body2"
+                  >
+                    Sign in on the internal platform
+                  </Typography>
+                </Box>
+                <Grid
+                  container
+                  spacing={3}
+                >
+                  <Grid
+                    item
+                    xs={12}
+                    md={6}
+                  >
+                    <Button
+                      color="primary"
+                      fullWidth
+                      startIcon={<FacebookIcon />}
+                      onClick={handleSubmit}
+                      size="large"
+                      variant="contained"
+                    >
+                      Login with Facebook
+                    </Button>
+                  </Grid>
+                  <Grid
+                    item
+                    xs={12}
+                    md={6}
+                  >
+                    <Button
+                      fullWidth
+                      startIcon={<GoogleIcon />}
+                      onClick={handleSubmit}
+                      size="large"
+                      variant="contained"
+                    >
+                      Login with Google
+                    </Button>
+                  </Grid>
+                </Grid>
+                <Box
+                  mt={3}
+                  mb={1}
+                >
+                  <Typography
+                    align="center"
+                    color="textSecondary"
+                    variant="body1"
+                  >
+                    or login with email address
+                  </Typography>
+                </Box>
+                <TextField
+                  error={Boolean(touched.email && errors.email)}
+                  fullWidth
+                  helperText={touched.email && errors.email}
+                  label="Email Address"
+                  margin="normal"
+                  name="email"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="email"
+                  value={values.email}
+                  variant="outlined"
+                />
+                <TextField
+                  error={Boolean(touched.password && errors.password)}
+                  fullWidth
+                  helperText={touched.password && errors.password}
+                  label="Password"
+                  margin="normal"
+                  name="password"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  type="password"
+                  value={values.password}
+                  variant="outlined"
+                />
+                <Box my={2}>
+                  <Button
+                    color="primary"
+                    disabled={isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Sign in now
+                  </Button>
+                </Box>
+                <Typography
+                  color="textSecondary"
+                  variant="body1"
+                >
+                  Don&apos;t have an account?
+                  {' '}
+                  <Link
+                    component={RouterLink}
+                    to="/register"
+                    variant="h6"
+                  >
+                    Sign up
+                  </Link>
+                  <AlertMessage status={status} response={response} clickButton={clickButton} />
+                </Typography>
+              </form>
+            )}
+          </Formik>
+        </Container>
+      </Box>
+    </Page>
+  );
+};
+
+export default LoginView;
